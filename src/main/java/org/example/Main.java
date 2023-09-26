daaa933ede8a3e5d420e6ac73af7b3e0562dd9b4
John <ashton.basile@gmail.com>
2023-09-26 11:42:41 +0100

    scoped function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("amanda", "bob", "charlie", "dave");
 
-        names.stream().map(String::toUpperCase).forEach(name -> System.out.println(name));
+        names.stream().map(String::toUpperCase).forEach(System.out::println);
     }
 }
